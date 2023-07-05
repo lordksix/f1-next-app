aa94ee9ca5ad282777a2e840398d850f4ba73e07
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-05 15:54:01 -0500

    Fix not-found page typo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function NotFound() {
     return (
         <div className="text-center">
-            <p className="mt-10">Sorry, the requested post does not exist.</p>
+            <p className="mt-10">Sorry, the requested page does not exist.</p>
             <Link href="/">Back to Home</Link>
         </div>
     )
