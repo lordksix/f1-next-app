0a8525c6ef6d3c5127ed6d2063e6a98df85b452d
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-06 15:06:45 -0500

    Add function to fetch races schedule

--- src/lib/getF1data.ts
+++ src/lib/getF1data.ts
@@ -24,8 +24,8 @@ export const getLastResultF1 = async (): Promise<ResultGPF1[] | undefined> => {
 };
 
 export const getRaceResultF1 = async (raceId: string): Promise<ResultGPF1[] | undefined> => {
-  const LASTEST_RESULT_ENDPOINT = `http://ergast.com/api/f1/${raceId}/results.json`;
-  const res = await fetch(LASTEST_RESULT_ENDPOINT);
+  const RESULT_ENDPOINT = `http://ergast.com/api/f1/${raceId}/results.json`;
+  const res = await fetch(RESULT_ENDPOINT);
 
   if (!res.ok) return undefined;
 
@@ -38,6 +38,19 @@ export const getRaceResultF1 = async (raceId: string): Promise<ResultGPF1[] | un
 export const getNextRaceF1 = async (): Promise<RaceGPF1[] | undefined>  => {
   const LASTEST_RESULT_ENDPOINT = 'https://ergast.com/api/f1/current/next.json';
   const res = await fetch(LASTEST_RESULT_ENDPOINT);
+
+  if (!res.ok) return undefined;
+
+  const { MRData } = await res.json();
+  const lastestResult: RaceGPF1[] = MRData.RaceTable.Races;
+
+  return lastestResult;
+};
+
+export const getRaceF1 = async (raceId: string): Promise<RaceGPF1[] | undefined>  => {
+  const RACE_ENDPOINT = `https://ergast.com/api/f1/${raceId}.json`;
+  console.log(RACE_ENDPOINT);
+  const res = await fetch(RACE_ENDPOINT);
 
   if (!res.ok) return undefined;
 
