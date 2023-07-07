42497be40323020c2d958c0aba377654f851cf73
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-07 06:10:16 -0500

    Update NavbarData.tsx

--- src/components/layout/NavbarData.tsx
+++ src/components/layout/NavbarData.tsx
@@ -1,21 +1,9 @@
-import { getLastResultF1, getNextRaceF1 } from '@/lib/getF1data';
+import navpaths from '@/lib/navbarpaths';
 import { nanoid } from '@reduxjs/toolkit';
 import Link from 'next/link';
 
 const NavbarData = async() => {
-  const lastResult = await getLastResultF1();
-  const nextRace = await getNextRaceF1();
-  let hrefLastResult: string;
-  let hrefNexTRace: string;
-  if(!lastResult) hrefLastResult = '/results';
-  else hrefLastResult = `/results/${lastResult[0].season}/${lastResult[0].round}`;
-  if(!nextRace) hrefNexTRace = '/races';
-  else hrefNexTRace = `/races/${nextRace[0].season}/${nextRace[0].round}`;
-  const links = [
-    { path: '/', text: 'Home' },
-    { path: hrefLastResult, text: 'Last Race' },
-    { path: hrefNexTRace, text: 'Next Race' },
-  ];
+  const links = await navpaths();
 
   const navbar = (
     links.map((link) => (
