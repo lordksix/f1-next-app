79a43e8e517b264442fc8b8b99f7c5b3f98a8053
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-07 06:10:38 -0500

    Update HeaderData.tsx

--- src/components/layout/HeaderData.tsx
+++ src/components/layout/HeaderData.tsx
@@ -6,12 +6,11 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import React from 'react'
 import useScroll from '@/lib/hooks/use-scroll';
 import Image from 'next/image';
-import useWindowSize from '@/lib/hooks/use-window-size';
-import NavbarData from './NavbarData';
+import { useMenuModal } from './MenuModal';
 
 const HeaderData = () => {
   const scrolled = useScroll(50);
-  const { isMobile } = useWindowSize();
+  const { MenuModal, setShowMenuModal } = useMenuModal();
 
   const returnBtn = (
     <Link
@@ -33,31 +32,35 @@ const HeaderData = () => {
       height="30"
       className="mr-2 rounded-sm bg-black dark:bg-transparent"
       ></Image>
+      F1Mania
     </Link>
   );
   const burgerBtnMenuModal = (
     <button
     className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-    onClick={() => console.log('true')}
+    onClick={() => setShowMenuModal(true)}
   >
     <GiHamburgerMenu />
   </button>
   );
   return (
-    <header
-      className={`fixed top-0 w-full flex justify-center ${
-        scrolled
-          ? "border-b border-gray-600  dark:border-gray-200 bg-white/50 dark:bg-black/50 backdrop-blur-xl"
-          : "bg-white/0 dark:bg-black/0"
-      } z-30 transition-all`}
-    >
-      <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-        {returnBtn}
-        {logo}
-        {isMobile ? burgerBtnMenuModal : <NavbarData />}
-      </div>
-    </header>
-  )
+    <>
+      <MenuModal />
+      <header
+        className={`fixed top-0 w-full flex justify-center ${
+          scrolled
+            ? "border-b border-gray-600  dark:border-gray-200 bg-white/50 dark:bg-black/50 backdrop-blur-xl"
+            : "bg-white/0 dark:bg-black/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+          {returnBtn}
+          {logo}
+          {burgerBtnMenuModal}
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default HeaderData
+export default HeaderData;
