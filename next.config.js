b593efb5b8b6902fc57930df941cbe5cf5c75587
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-07 06:42:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   staticPageGenerationTimeout: 1000,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/lordksix/f1-next-app",
+        permanent: false,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
