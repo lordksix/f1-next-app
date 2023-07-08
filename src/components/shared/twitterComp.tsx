2284114bf2799cefc2c1db29dee22ede6bf22f33
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-08 06:26:31 -0500

    Add twitter timeline

---
+++ src/components/shared/twitterComp.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+
+const TwitterComp = () => {
+  return (
+    <TwitterTimelineEmbed
+      sourceType="profile"
+      screenName="f1"
+      ariaPolite="assertive"
+      theme="dark"
+      onLoad={function noRefCheck(){}}
+      options={{ height: 400, width: 400}}
+    />
+  );
+};
+
+export default TwitterComp;
