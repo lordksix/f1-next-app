5e1a87ecb6fd22baeb49ef4b6ca9534e96282cbc
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-18 20:33:35 -0500

    Improve profile page

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -8,7 +8,7 @@ function ProfileLayout({
   return (
     <>
       <HeaderNav />
-      <main className="flex flex-col items-center justify-center w-10/12 max-w-xl min-h-screen py-32 mx-auto no-scrollbar md:w-10/12 g:w-9/12">
+      <main className="flex flex-col w-10/12 max-w-2xl min-h-screen pt-20 mx-auto no-scrollbar md:w-10/12 g:w-9/12">
         {children}
       </main>
     </>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,24 +5,23 @@ import { redirect } from 'next/navigation'
 
 export default async function ProfilePage() {
   const session = await getServerSession(options)
+
   if (!session) {
-    redirect('/api/auth/signin?callbackUrl=/server')
+    redirect('/api/auth/signin?callbackUrl=/profile')
   }
   const { user } = session;
 
-
   return (
-    <section className="min-h-screen pt-20 bg-ct-blue-600">
-      <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
+    <section className="w-full h-full py-4 rounded-md bg-ct-blue-600">
+      <div className="flex justify-center w-10/12 h-full pt-4 mx-auto text-black rounded-md bg-ct-dark-100">
         <div>
           <p className="mb-3 text-5xl font-semibold text-center">
             Profile Page
           </p>
           <div className="mt-8">
-            <p className="mb-3">Id: {user?.id}</p>
             <p className="mb-3">Name: {user?.name}</p>
             <p className="mb-3">Email: {user?.email}</p>
-            <p className="mb-3">Role: {user?.role}</p>
+            <p className="mb-3">Role: {user?.role || 'user'}</p>
             <p className="mb-3">Verified: {String(user?.emailVerified)}</p>
           </div>
         </div>
