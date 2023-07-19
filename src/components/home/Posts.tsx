1da40b01c975048ffb1196e4be05d17b68fb01e7
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-18 20:34:05 -0500

    Change news modal component

--- src/components/home/Posts.tsx
+++ src/components/home/Posts.tsx
@@ -1,6 +1,7 @@
 import { getPostsMeta } from "@/lib/posts"
 import ListItem from "./ListItem"
 import Link from "next/link"
+import { f1tffBlack } from "@/app/fonts"
 
 export default async function Posts() {
   const posts = await getPostsMeta()
@@ -10,14 +11,14 @@ export default async function Posts() {
   }
 
   return (
-    <section className="max-w-2xl mx-auto">
-      <h2 className="text-lg font-bold text-center md:text-xl xl:text-3xl">News</h2>
+    <section className="max-w-2xl px-2 py-4 mx-auto bg-red-600 rounded-3xl">
+      <h2 className={`${f1tffBlack.variable} text-lg font-bold text-center md:text-xl xl:text-3xl`}>News</h2>
       <ul className="w-full p-0 list-none">
         {posts.slice(0, 2).map(post => (
             <ListItem key={post.id} post={post} />
         ))}
       </ul>
-      <Link href={'/posts'} className="w-full mt-4 font-bold text-right hover:text-red-600" >More news</Link>
+      <Link href={'/posts'} className="w-full mt-4 font-bold hover:text-black" >More news</Link>
     </section>
   )
 }
